9ff0e15e8088f989ad178a35bfb31a1b6be32075
sthapitsrijeet <sthapitsrijeet@gmail.com>
2024-02-01 20:53:54 -0500

    Initial

---
+++ FitConnect/FitConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitConnectApp.swift
+//  FitConnect
+//
+//  Created by Srijeet Sthapit on 2024-02-01.
+//
+
+import SwiftUI
+
+@main
+struct FitConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
