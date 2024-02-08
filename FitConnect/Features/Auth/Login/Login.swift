04a577444eaeae520b01b18d67c4916a50453962
sthapitsrijeet <sthapitsrijeet@gmail.com>
2024-02-08 00:26:11 -0500

    Fixes and validation updates

--- FitConnect/Features/Auth/Login/Login.swift
+++ FitConnect/Features/Auth/Login/Login.swift
@@ -12,15 +12,18 @@ import FirebaseAuth
 struct Login: View {
     @State var email: String = ""
     @State var password: String = ""
+    @State private var isEmailValid: Bool = false
+    @State private var isPasswordValid: Bool = false
+    @State private var isLoginButtonDisabled: Bool = true
     @State var path = NavigationPath()
-
+    
     var body: some View {
         ZStack {
             Color(hex: 0xF2F2F2) // Background color
                 .edgesIgnoringSafeArea(.all)
+                .ignoresSafeArea()
             
             VStack {
-                
                 
                 Image("Logo") // Replace "your_logo" with the name of your logo image asset
                     .resizable()
@@ -39,35 +42,45 @@ struct Login: View {
                     .padding(.bottom,100)
                     .overlay(
                         VStack(spacing: 15) {
+                    
                             TextField("Email", text: $email)
-                                .textFieldStyle(.roundedBorder)
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(Color.gray.opacity(0.5), lineWidth: 2)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(8)
+                                .padding(.horizontal, 20)
+                                .onChange(of: email,initial:true) { value,newValue in
+                                    isEmailValid = isValidEmail(newValue)
+                                    updateLoginButtonState()
                                 }
-                                .padding(.horizontal)
                             
                             SecureField("Password", text: $password)
-                                .textFieldStyle(.roundedBorder)
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(Color.gray.opacity(0.5), lineWidth: 2)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(8)
+                                .padding(.horizontal, 20)
+                                .onChange(of: password, initial:true) {value, newValue in
+                                    isPasswordValid = isValidPassword(newValue)
+                                    updateLoginButtonState()
                                 }
-                                .padding(.horizontal)
                             
-                            Button("Login") {
-                                loginWithEmailPassword(email: email, password: password)
-                            }
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color(hex: 0x00AB53))
-                            .cornerRadius(10)
+                            Button(action: login, label: {
+                                Text("Login")
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(isLoginButtonDisabled ? Color.gray.opacity(0.5) : Color("Primary"))
+                                    .cornerRadius(8)
+                                    .padding(.horizontal, 20)
+                            })
+                            .disabled(isLoginButtonDisabled)
+                            .padding(.top, 20)
+                            
                             
                             HStack {
                                 Text("Not a member yet?")
                                 Button("Register") {
                                     print("Here")
-                                    path.append("Register")
+                                    
                                 }
                             }
                         }
@@ -76,21 +89,55 @@ struct Login: View {
             .padding()
         }
     }
-
-    // Function to handle login with email and password
+    private func login() {
+        loginWithEmailPassword(email: email, password: password)
+        print("Login with email: \(email) and password: \(password)")
+    }
+    
+    private func updateLoginButtonState() {
+        isLoginButtonDisabled = !isEmailValid || !isPasswordValid
+    }
+    
     func loginWithEmailPassword(email: String, password: String) {
+            // Validate email format
+        guard isValidEmail(email) else {
+            print("Invalid email format")
+            return
+        }
+        
+            // Validate password
+        guard isValidPassword(password) else {
+            print("Invalid password")
+            return
+        }
+        
+            // Proceed with authentication
         Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
             if let error = error {
                 print("Login error:", error.localizedDescription)
-                // Handle login errors here
+                    // Handle login errors here
                 return
             }
-            // User successfully logged in
+                // User successfully logged in
             if let user = authResult?.user {
                 print("User logged in:", user)
-                // You can redirect or perform any other actions here after successful login
+                    // You can redirect or perform any other actions here after successful login
             }
         }
+    }
+    
+        // Function to validate email format
+    func isValidEmail(_ email: String) -> Bool {
+            // Basic email format validation
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+    
+        // Function to validate password strength
+    func isValidPassword(_ password: String) -> Bool {
+            // Basic password length validation
+        return password.count >= 6 // You can add more complex validation rules as needed
     }
 }
 
--- FitConnect/Features/Auth/Register/Register.swift
+++ FitConnect/Features/Auth/Register/Register.swift
@@ -12,19 +12,61 @@ import FirebaseFirestore
 
 struct Register: View {
     @State var fullname: String = ""
-    @State var address: String = ""
     @State var contactNumber: String = ""
-    @State var username: String = ""
     @State var email: String = ""
     @State var password: String = ""
     @State var confirmPassword: String = ""
+    @State private var isFullNameValid: Bool = true
+    @State private var isEmailValid: Bool = true
+    @State private var isContactValid: Bool = true
+    @State private var isPasswordValid: Bool = true
+    @State private var isConfirmPasswordValid: Bool = true
+    @State private var isRegisterButtonDisabled: Bool = true
+    
     let db = Firestore.firestore()
-
+    private func isValidFullName(_ fullName: String) -> Bool {
+            // Simple validation for full name (for example, at least 3 characters)
+        return fullName.count >= 3
+    }
+    
+    private func isValidEmail(_ email: String) -> Bool {
+            // Simple email validation
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
+    }
+    
+    private func isValidContact(_ contact: String) -> Bool {
+            // Simple validation for contact number (for example, at least 10 digits)
+        return contact.count >= 10
+    }
+    
+    private func isValidPassword(_ password: String) -> Bool {
+            // Simple password validation (for example, at least 6 characters)
+        return password.count >= 6
+    }
+    
+    private func isValidConfirmPassword(_ confirmPassword: String) -> Bool {
+            // Simple validation to check if confirmPassword matches password
+        return confirmPassword == password
+    }
+    
+    private func updateRegisterButtonState() {
+        isRegisterButtonDisabled = !isFullNameValid || !isEmailValid || !isContactValid || !isPasswordValid || !isConfirmPasswordValid
+    }
+    private func register() async {
+        await signUpAndAddUserData(email: email,
+                                   password: password,
+                                   fullname: fullname,
+                                   contactNumber: contactNumber
+        )
+        
+        
+    }
     var body: some View {
         ZStack {
             Color(hex: 0xF2F2F2) // Background color
                 .edgesIgnoringSafeArea(.all)
-            
+                .ignoresSafeArea()
             VStack {
                 Image("Logo") // Replace "your_logo" with the name of your logo image asset
                     .resizable()
@@ -38,77 +80,195 @@ struct Register: View {
                 
                 RoundedRectangle(cornerRadius: 20)
                     .fill(Color.white)
+                    .foregroundColor(Color.black)
                     .padding()
                     .overlay(
                         VStack(spacing: 22) {
-                            FCTextField(placeholder: "Fullname", value: $fullname)
-                                .frame(height: 30)
-                                .frame(width: 280)
+                            TextField("Full Name", text: $fullname)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(8)
+                                .padding(.horizontal, 20)
+                                .foregroundColor(.black)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(isFullNameValid ? Color.clear : Color.red, lineWidth: 2)
+                                        .padding(.horizontal, 20)// Border color changes based on the error state
+                                )
+                                .onChange(of: fullname, perform: { newValue in
+                                    isFullNameValid = isValidFullName(newValue)
+                                    updateRegisterButtonState()
+                                })
                             
-                            FCTextField(placeholder: "Address", value: $address)
-                                .frame(height: 30)
-                                .frame(width: 280)
-                            
-                            FCTextField(placeholder: "Contact number", value: $contactNumber)
-                                .frame(height: 30)
-                                .frame(width: 280)
-                            
-                            FCTextField(placeholder: "Email", value: $email)
-                                .frame(height: 30)
-                                .frame(width: 280)
-                            
-                            FCTextField(placeholder: "Username", value: $username)
-                                .frame(height: 30)
-                                .frame(width: 280)
-                            
-                            FCTextField(placeholder: "Password", value: $password)
-                                .frame(height: 30)
-                                .frame(width: 280)
-                            
-                            FCTextField(placeholder: "Confirm Password", value: $confirmPassword)
-                                .frame(height: 30)
-                                .frame(width: 280)
-                            
-                            Button("Register") {
-                                Task {
-                                    do {
-                                        let authResult = try await Auth.auth().createUser(withEmail: email, password: password)
-                                        if let user = Auth.auth().currentUser {
-                                            let uid = user.uid
-                                            do {
-                                                try await Firestore.firestore().collection("users").document(uid).setData([
-                                                    "email": email,
-                                                    "fullName": fullname,
-                                                    "address": address,
-                                                    "contactNumber": contactNumber,
-                                                    "userName": username
-                                                ])
-                                                print("Sign-up and Firestore data addition successful")
-                                            } catch {
-                                                print("Firestore error: \(error.localizedDescription)")
-                                            }
-                                        }
-                                    } catch {
-                                        print("Sign-up error: \(error.localizedDescription)")
-                                    }
-                                }
+                            if !isFullNameValid {
+                                Text("Must be greater than 3 characters")
+                                    .foregroundColor(.red)
+                                    .font(.caption) // Smaller font size
+                                
                             }
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color(hex: 0x00AB53))
-                            .cornerRadius(10)
                             
+                            TextField("Email", text: $email)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(8)
+                                .padding(.horizontal, 20)
+                                .foregroundColor(.black)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(isEmailValid ? Color.clear : Color.red, lineWidth: 2)
+                                        .padding(.horizontal, 20)// Border color changes based on the error state
+                                )
+                                .onChange(of: email, perform: { newValue in
+                                    isEmailValid = isValidEmail(newValue)
+                                    updateRegisterButtonState()
+                                })
+                            if !isEmailValid {
+                                Text("Invalid email format")
+                                    .foregroundColor(.red)
+                                    .font(.caption) // Smaller font size
+                             
+                            }
+                            
+                            
+                            TextField("Contact", text: $contactNumber)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(8)
+                                .padding(.horizontal, 20)
+                                .foregroundColor(.black)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(isContactValid ? Color.clear : Color.red, lineWidth: 2)
+                                        .padding(.horizontal, 20)// Border color changes based on the error state
+                                )
+                                .onChange(of: contactNumber, perform:{ newValue in
+                                    isContactValid = isValidContact(newValue)
+                                    updateRegisterButtonState()
+                                })
+                            if !isContactValid {
+                                Text("Invalid phone number")
+                                    .foregroundColor(.red)
+                                    .font(.caption) // Smaller font size
+                                
+                            }
+                            
+                            SecureField("Password", text: $password)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(8)
+                                .padding(.horizontal, 20)
+                                .foregroundColor(.black)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke((isPasswordValid) ? Color.clear : Color.red, lineWidth: 2)
+                                        .padding(.horizontal, 20)// Border color changes based on the error state
+                                )
+                                .onChange(of: password, perform: { newValue in
+                                    isPasswordValid = isValidPassword(newValue)
+                                    updateRegisterButtonState()
+                                })
+                            if !isPasswordValid {
+                                Text("Invalid phone number")
+                                    .foregroundColor(.red)
+                                    .font(.caption) // Smaller font size
+                                
+                            }
+                            
+                            SecureField("Confirm Password", text: $confirmPassword)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(8)
+                                .padding(.horizontal, 20)
+                                .foregroundColor(.black)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(( isConfirmPasswordValid) ? Color.clear : Color.red, lineWidth: 2)
+                                        .padding(.horizontal, 20)// Border color changes based on the error state
+                                )
+                                .onChange(of: confirmPassword, perform: { newValue in
+                                    isConfirmPasswordValid = isValidConfirmPassword(newValue)
+                                    updateRegisterButtonState()
+                                })
+                            if !isConfirmPasswordValid {
+                                Text("Invalid phone number")
+                                    .foregroundColor(.red)
+                                    .font(.caption) // Smaller font size
+                                
+                            }
+                            
+                            Button(action: {Task{await register()}}, label: {
+                                Text("Register")
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(isRegisterButtonDisabled ? Color.gray.opacity(0.5) : Color("Primary"))
+                                    .cornerRadius(8)
+                                    .padding(.horizontal, 20)
+                            })
+                            .disabled(isRegisterButtonDisabled)
+                            .padding(.top, 20)
                             HStack {
-                                Text("Already a member?")
+                                Text("Already member?")
+                                    .foregroundColor(.black)
+                                Button("Login") {
+                                    print("Here")
+                                    
+                                }.foregroundColor(.black)
                             }
                         }
+                            .foregroundColor(.white)
+                            .padding()
+                            .cornerRadius(10)
+                        
                     )
             }
-            .padding()
+           
         }
+    
+        
     }
 }
 
+
+
+
+
+func signUpAndAddUserData(email: String, password: String, fullname: String, contactNumber: String ) async {
+    do {
+        let authResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        
+        if let user = Auth.auth().currentUser {
+            let uid = user.uid
+            
+                // Perform Firestore data addition concurrently with authentication
+            async let firestoreTask: () = addUserDataToFirestore(uid: uid, email: email, fullname: fullname, contactNumber: contactNumber)
+            
+                // Wait for both tasks to complete
+            _ = try await (authResult, firestoreTask)
+            
+            print("Sign-up and Firestore data addition successful")
+        }
+    } catch {
+        print("Sign-up error: \(error.localizedDescription)")
+    }
+    
+}
+
+func addUserDataToFirestore(uid: String, email: String, fullname: String, contactNumber: String) async throws {
+    do {
+        try await Firestore.firestore().collection("users").document(uid).setData([
+            "email": email,
+            "fullName": fullname,
+            "contactNumber": contactNumber,
+            
+        ])
+        
+        print("Firestore data addition successful")
+    } catch {
+        throw error
+    }
+    
+}
 struct Register_Previews: PreviewProvider {
     static var previews: some View {
         Register()
