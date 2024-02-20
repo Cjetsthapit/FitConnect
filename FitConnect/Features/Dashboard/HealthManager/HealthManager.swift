ba253bf0f9db472fee640e6bad9bfac1c859f90b
Nibha Maharjan <nibhamaharjan@yahoo.com>
2024-02-19 22:04:52 -0500

    Running Add

--- FitConnect/Features/Dashboard/HealthManager/HealthManager.swift
+++ FitConnect/Features/Dashboard/HealthManager/HealthManager.swift
@@ -12,15 +12,28 @@ extension Date{
     static var startOfDay: Date{
         Calendar.current.startOfDay(for: Date())
     }
+    static var startOfWeek: Date{
+        let calendar = Calendar.current
+        var components = calendar.dateComponents([.yearForWeekOfYear,.weekOfYear], from: Date())
+        components.weekday = 2 // Monday
+        return calendar.date(from: components)!
+    }
 }
 
 class HealthManager: ObservableObject{
     let healthStore = HKHealthStore()
     
     @Published var activities: [String: Activity] = [:]
+    
+    @Published var mockActivities: [String: Activity] = [
+        "todaySteps": Activity(id: 0, title: "Todays steps", subtitle: "Goal 10,000", image: "figure.walk", amount:"10000"),
+        "todayCalories": Activity(id: 1, title: "Todays Calories", subtitle: "Goal 900", image: "figure.step.training", amount:"2000")
+    ]
+    
     init(){
         let steps = HKQuantityType(.stepCount)
         let calories = HKQuantityType(.activeEnergyBurned)
+        let workout = HKObjectType.workoutType()
         let healthTypes: Set = [steps, calories]
         
         Task{
@@ -28,6 +41,7 @@ class HealthManager: ObservableObject{
                 try await healthStore.requestAuthorization(toShare: [], read: healthTypes)
                 fetchTodaySteps()
                 fetchTodayCalories()
+                fetchWeekRunningStats()
             }catch{
                 print ("error fetching health data")
             }
@@ -63,12 +77,35 @@ class HealthManager: ObservableObject{
                 return
             }
             let calorieCount = quantity.doubleValue(for: .kilocalorie())
-            let activity = Activity(id: 1, title: "Todays Calories", subtitle: "Goal 900", image: "figure.flame", amount: calorieCount.formattedString())
+            let activity = Activity(id: 1, title: "Todays Calorie", subtitle: "Goal 900", image: "figure.step.training", amount: calorieCount.formattedString())
             
             DispatchQueue.main.async{
                 self.activities["todayCalories"] = activity
             }
             print (calorieCount)
+        }
+        healthStore.execute(query)
+    }
+    func fetchWeekRunningStats(){
+        let workout = HKSampleType.workoutType()
+        let timePredicate = HKQuery.predicateForSamples(withStart: .startOfWeek, end: Date())
+        let workoutPredicate = HKQuery.predicateForWorkouts(with: .running)
+        let predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [timePredicate,workoutPredicate])
+        let query = HKSampleQuery(sampleType: workout, predicate: predicate, limit: 25, sortDescriptors: nil) { _, sample, error in
+            guard let workouts = sample as? [HKWorkout], error == nil else {
+                print ("error fetchin todays calories")
+                return
+            }
+            var count:Int = 0
+            for workout in workouts {
+                let duration = Int(workout.duration)/60
+                count += duration
+            }
+            let activity = Activity(id: 2, title: "Running", subtitle: "Mins ran this week", image: "figure.walk", amount: "\(count) minutes")
+            
+            DispatchQueue.main.async{
+                self.activities["weekRunning"] = activity
+            }
         }
         healthStore.execute(query)
     }
--- FitConnect/Features/Dashboard/Views/ActivityView.swift
+++ FitConnect/Features/Dashboard/Views/ActivityView.swift
@@ -10,8 +10,19 @@ import SwiftUI
 
 struct ActivityView: View{
     @EnvironmentObject var manager: HealthManager
+    let welcomeArray = ["Welcome","Bienvenue","Bienvenido"]
+    @State private var currentIndex = 0
+    
     var body: some View {
-        VStack{
+        VStack(alignment:.leading){
+            Text(welcomeArray[currentIndex])
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.secondary)
+                .animation(.easeInOut(duration: 1), value:currentIndex)
+                .onAppear{
+                    startWelcomeTimer()
+                }
             LazyVGrid(columns: Array(repeating: GridItem(spacing:20), count: 2)) {
                 ForEach(manager.activities.sorted(by:  {$0.value.id < $1.value.id}), id:\.key){item in
                     ActivityCard(activity:item.value)
@@ -20,15 +31,21 @@ struct ActivityView: View{
             }
                 .padding(.horizontal)
         }
-        .onAppear{
-            manager.fetchTodaySteps()
-            manager.fetchTodayCalories()
+        .frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .top)
+        }
+    func startWelcomeTimer(){
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true){
+            _ in withAnimation{
+                currentIndex = (currentIndex + 1) % welcomeArray.count
+            }
         }
     }
-}
+    }
+
 
 struct ActivityView_Previews: PreviewProvider{
     static var previews: some View{
         ActivityView()
+            .environmentObject(HealthManager())
     }
 }
