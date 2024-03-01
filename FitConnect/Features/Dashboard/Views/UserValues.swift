f477cbccc02cb17f8084adad8d502848e839c1bd
sthapitsrijeet <sthapitsrijeet@gmail.com>
2024-02-29 23:57:11 -0500

    Added gitignore

---
+++ FitConnect/Features/Dashboard/Views/UserValues.swift
@@ -0,0 +1,18 @@
+//
+//  UserValues.swift
+//  FitConnect
+//
+//  Created by Srijeet Sthapit on 2024-02-29.
+//
+
+import SwiftUI
+
+struct UserValues: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserValues()
+}
