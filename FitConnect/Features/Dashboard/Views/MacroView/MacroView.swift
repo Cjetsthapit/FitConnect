ff322e97173ad41b7a1b9f1bf567df1a16cfa35f
sthapitsrijeet <sthapitsrijeet@gmail.com>
2024-03-01 00:29:38 -0500

    Minor fix

--- FitConnect/Features/Dashboard/Views/MacroView/MacroView.swift
+++ FitConnect/Features/Dashboard/Views/MacroView/MacroView.swift
@@ -1,9 +1,3 @@
-    //
-    //  MacroView.swift
-    //  FitConnect
-    //
-    //  Created by Srijeet Sthapit on 2024-02-15.
-    //
 
 import SwiftUI
 
@@ -14,74 +8,79 @@ struct MacroView: View {
     @State var carbValue: Double = 0.0
     @State var proteinValue: Double = 0.0
     @State var fatValue: Double = 0.0
-    @EnvironmentObject var fitConnect:  FitConnectData
+    @EnvironmentObject var fitConnect: FitConnectData
+    
     var body: some View {
-        VStack{
-            Text("Today's intake")
-            HStack(){
-                CircularProgressBar(progress: self.$fitConnect.totalCarb, title: "Carbs")
-                    .padding()
-                
-                CircularProgressBar(progress: self.$fitConnect.totalProtein, title: "Protein")
-                    .padding()
-                
-                CircularProgressBar(progress: self.$fitConnect.totalFat, title: "Fats")
-                    .padding()
-
-            }
-            List {
-                ForEach(fitConnect.fitConnectData?.food ?? [], id: \.self) { macro in
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("\(macro.food.capitalized)")
-                            .font(.headline)
-                        
-                        HStack(spacing: 16) {
-                            VStack(alignment: .leading, spacing: 4) {
-                                HStack {
-                                    Image(systemName: "flame")
-                                    Text("Fat:")
-                                }
-                                HStack {
-                                    Image(systemName: "leaf")
-                                    Text("Protein:")
-                                }
-                                HStack {
-                                    Image(systemName: "tennis.racket.circle")
-                                    Text("Carb:")
-                                }
-                            }
+        NavigationView {
+            VStack {
+                Text("Today's intake")
+                HStack {
+                    CircularProgressBar(progress: self.$fitConnect.totalCarb, title: "Carbs")
+                        .padding()
+                    
+                    CircularProgressBar(progress: self.$fitConnect.totalProtein, title: "Protein")
+                        .padding()
+                    
+                    CircularProgressBar(progress: self.$fitConnect.totalFat, title: "Fats")
+                        .padding()
+                }
+                List {
+                    ForEach(fitConnect.fitConnectData?.food ?? [], id: \.self) { macro in
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("\(macro.food.capitalized)")
+                                .font(.headline)
                             
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text("\(macro.fat.formattedString()) gm")
-                                    .font(.subheadline)
-                                Text("\(macro.protein.formattedString()) gm")
-                                    .font(.subheadline)
-                                Text("\(macro.carb.formattedString()) gm")
-                                    .font(.subheadline)
+                            HStack(spacing: 16) {
+                                VStack(alignment: .leading, spacing: 4) {
+                                    HStack {
+                                        Image(systemName: "flame")
+                                        Text("Fat:")
+                                    }
+                                    HStack {
+                                        Image(systemName: "leaf")
+                                        Text("Protein:")
+                                    }
+                                    HStack {
+                                        Image(systemName: "tennis.racket.circle")
+                                        Text("Carb:")
+                                    }
+                                }
+                                
+                                VStack(alignment: .leading, spacing: 4) {
+                                    Text("\(macro.fat.formattedString()) gm")
+                                        .font(.subheadline)
+                                    Text("\(macro.protein.formattedString()) gm")
+                                        .font(.subheadline)
+                                    Text("\(macro.carb.formattedString()) gm")
+                                        .font(.subheadline)
+                                }
                             }
                         }
                     }
                 }
+                .padding()
             }
-            .padding()
-                .toolbar{
-                    ToolbarItem{
-                        Button{
-                            showingForm.toggle()
-                        }
-                    label: {
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {
+                        showingForm.toggle()
+                    }) {
                         Text("Add Data")
                     }
-                    }
                 }
-                .toolbar(.visible, for: .navigationBar)
-        }
-        .sheet(isPresented: $showingForm) {
-            AddMacro(foodName: self.$foodName, date: self.$date, showingForm: self.$showingForm)
+            }
+            .sheet(isPresented: $showingForm) {
+                AddMacro(foodName: self.$foodName, date: self.$date, showingForm: self.$showingForm)
+            }
+            .onAppear() {
+                print("Total carb: ", fitConnect.totalCarb)
+            }
         }
     }
 }
 
-#Preview {
-    MacroView()
+struct MacroView_Previews: PreviewProvider {
+    static var previews: some View {
+        MacroView()
+    }
 }
