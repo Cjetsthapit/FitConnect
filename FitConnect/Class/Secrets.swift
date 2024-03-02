2fa632c34bd20d17748731624e9cf9397ea3fd12
sthapitsrijeet <sthapitsrijeet@gmail.com>
2024-03-01 20:32:13 -0500

    Fix

---
+++ FitConnect/Class/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  FitConnect
+//
+//  Created by Srijeet Sthapit on 2024-02-14.
+//
+
+import Foundation
+
+enum  Secrets{
+    static let apiKey = "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"
+}
