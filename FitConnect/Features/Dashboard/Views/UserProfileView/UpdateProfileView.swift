2a3813f03e7548e0c5d85cd0c0ef44b3290debdb
Nibha Maharjan <nibhamaharjan@yahoo.com>
2024-03-13 21:34:54 -0400

    New user profile view and update profile view added

---
+++ FitConnect/Features/Dashboard/Views/UserProfileView/UpdateProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateProfileView.swift
+//  FitConnect
+//
+//  Created by Nibha Maharjan on 2024-03-13.
+//
+
+import SwiftUI
+
+struct UpdateProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateProfileView()
+}
---
+++ FitConnect/Features/Dashboard/Views/UserProfileView/UserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileView.swift
+//  FitConnect
+//
+//  Created by Nibha Maharjan on 2024-03-13.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
