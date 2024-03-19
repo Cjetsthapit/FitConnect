7ec621fa0054ca5eb0f8b11a48879bce9e0d1109
Nibha Maharjan <nibhamaharjan@yahoo.com>
2024-03-19 11:27:15 -0400

    Working on step history

---
+++ FitConnect/Features/Dashboard/Views/Chart/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  FitConnect
+//
+//  Created by Nibha Maharjan on 2024-03-18.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
